c8941eb3259cc42e20230b834d490fbf3c699537
Ckyiu <UnsignedArduino@outlook.com>
2022-09-06 12:56:16 -0400

    Make simple map

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+function define_maps () {
+    maps = [tilemap`classic_loop_map`]
+    driving_tiles = [tilemap`classic_loop_map_driving_tiles`]
+    slow_tiles = [tilemap`classic_loop_map_slow_tiles`]
+    wall_tiles = [tilemap`classic_loop_map_wall_tiles`]
+    map_names = ["Classic loop"]
+    flower_seeds = [645]
+}
+let flower_seeds: number[] = []
+let map_names: string[] = []
+let wall_tiles: tiles.TileMapData[] = []
+let slow_tiles: tiles.TileMapData[] = []
+let driving_tiles: tiles.TileMapData[] = []
+let maps: tiles.TileMapData[] = []
+define_maps()
